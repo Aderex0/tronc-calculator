422484a6702b538ad14fac2aea5ab59359b34aa5
Andrius <Andriusj6@hotmail.com>
2020-02-04 15:56:04 +0000

    frontpage tests

--- cypress/integration/FrontPage.spec.js
+++ cypress/integration/FrontPage.spec.js
@@ -6,6 +6,9 @@ describe('Visits the website and checks if frontpage buttons works', () => {
   it('clicks the logo to return to frontpage', () => {
     cy.visit(url)
     cy.get('[data-testid="frontpage-return"]').click()
+  })
+  it('frontpage shoudl contain all the elements', () => {
     cy.get('.fp-text-container').should('have.length', 2)
+    cy.get('.intro-container').should('have.class', 'intro-text', 'required')
   })
 })
