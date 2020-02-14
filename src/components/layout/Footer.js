f16109e2e8d0bbaba0426762785ac482e8b9a223
Andrius <Andriusj6@hotmail.com>
2020-02-14 11:05:08 +0000

    images, resizing, tests, friday morning pt1

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (
     <>
       <div className='footer-container'>
-        <span className='extra-space'></span>
-        <div className='footer-options'>
-          <ul>
-            <li>Site map</li>
-            <li> Tronc Calculator</li>
-            <li> Earnings Calculator</li>
-            <li> Contact us</li>
-          </ul>
+        <div className='footer-options-container'>
+          <div className='footer-options'>
+            <span>Site map</span>
+            <Link to='/tools/tronc-calculator'>
+              <span data-testid='footer-tronc-calc'> Tronc Calculator</span>
+            </Link>
+            <Link to='/tools/earnings-calculator'>
+              <span data-testid='footer-earnings-calc'>
+                {' '}
+                Earnings Calculator
+              </span>
+            </Link>
+            <Link to=''>
+              <span data-testid='footer-contact-us'> Contact us</span>
+            </Link>
+          </div>
         </div>
         <div className='footer-logo'>
           <p>
